6757f1f4d7f2b5a97b39a92be04a6977d48340fa
Espen Moe-Nilssen <espen@medialog.no>
2013-06-18 16:26:39 +0200

    Added layer and some css to the new view

--- bda/plone/shopviews/browser/views.py
+++ bda/plone/shopviews/browser/views.py
@@ -1,26 +1,35 @@
-from zope.interface import implements, Interface
-
+from zope.interface import implements, Interface, Attribute
 from Products.Five import BrowserView
-from Products.CMFCore.utils import getToolByName
 
+#from bda.plone.cart.interfaces.ICartDataProvider import  get_data_provider
 
 from bda.plone.shopviews import shopviewsMessageFactory  as _
+
 
 class IProductsView(Interface):
     """
     Products  view interface
     """
-
+    
+    #def currency(self):
+    #    """Get the currency"""
+        
     def test():
         """ test method"""
 
+
+
+#I am not sure if this is the right way to define the view
 
 class ProductsView(BrowserView):
     """
     Products browser view
     """
     implements(IProductsView)
-
+    
+    #property
+    #def currency(self):
+    #    return self.data_provider.currency
 
     def test(self):
         """
