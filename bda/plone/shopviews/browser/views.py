97569c7b95ac1a56b5083d317f7066dd36a69605
Espen Moe-Nilssen <espen@medialog.no>
2013-07-15 14:13:06 +0200

    pdb again

--- bda/plone/shopviews/browser/views.py
+++ bda/plone/shopviews/browser/views.py
@@ -85,6 +85,7 @@ class ProductsView(BrowserView):
         #results = self.find_objects
         context = self.context
         catalog = getToolByName(self, 'portal_catalog')
+        import pdb; pdb.set_trace()
         if context.is_folderish: 
             folder_path = '/'.join(context.getPhysicalPath())
         else:
