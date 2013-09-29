127f91f22d05c7c83507cda25de948d4885e285f
Robert Niederreiter <office@squarewave.at>
2013-09-29 15:57:36 +0200

    work on views

--- bda/plone/productshop/browser/views.py
+++ bda/plone/productshop/browser/views.py
@@ -1,15 +1,30 @@
+from Acquisition import (
+    aq_inner,
+    aq_parent,
+)
 from zope.i18nmessageid import MessageFactory
 from Products.Five import BrowserView
 from Products.CMFCore.utils import getToolByName
-from ..interfaces import IProduct
+from ..interfaces import (
+    IProduct,
+    IProductGroup,
+)
 
 
 _ = MessageFactory('bda.plone.productshop')
 
 
-class Listing(BrowserView):
-    """Product Listing.
-    """
+def img_tag(context, scale, css_class):
+    if not context.image:
+        return None
+    scales = context.restrictedTraverse('@@images')
+    scale = scales.scale('image', scale)
+    if not scale:
+        return None
+    return scale.tag(css_class=css_class)
+
+
+class ProductListing(BrowserView):
     image_scale = 'thumb'
 
     @property
@@ -26,30 +41,73 @@ class Listing(BrowserView):
             obj = brain.getObject()
             if not IProduct.providedBy(obj):
                 continue
-            image = None
-            if obj.image:
-                scales = obj.restrictedTraverse('@@images')
-                scale = scales.scale('image', self.image_scale)
-                if scale:
-                    image = scale.tag(css_class='product_listing_image')
-            ret.append({
-               'obj': obj,
-               'preview': image,
-            })
+            item = dict()
+            item['obj'] = obj
+            item['preview'] = img_tag(
+                obj, self.image_scale, 'product_listing_image')
+            item['buyable_controls'] = True
+            if IProductGroup.providedBy(obj):
+                item['buyable_controls'] = False
+            ret.append(item)
         return ret
 
 
-class Product(BrowserView):
-    """Product view.
-    """
+class ProductView(BrowserView):
     image_scale = 'mini'
 
     @property
     def image(self):
-        if not self.context.image:
-            return None
-        scales = self.context.restrictedTraverse('@@images')
-        scale = scales.scale('image', self.image_scale)
-        if not scale:
-            return None
-        return scale.tag(css_class='product_image')
+        return img_tag(self.context, self.image_scale, 'product_image')
+
+    @property
+    def details(self):
+        return self.context.details
+
+    @property
+    def datasheet(self):
+        return self.context.datasheet
+
+    @property
+    def related_items(self):
+        if hasattr(self.context, 'relatedItems'):
+            return self.context.relatedItems
+        return None
+
+
+class VariantView(ProductView):
+
+    @property
+    def parent(self):
+        return aq_parent(aq_inner(self.context))
+
+    @property
+    def image(self):
+        context_image = img_tag(
+            self.context, self.image_scale, 'product_image')
+        if context_image:
+            return context_image
+        return img_tag(self.parent, self.image_scale, 'product_image')
+
+    @property
+    def details(self):
+        if self.context.details:
+            return self.context.details
+        return self.parent.details
+
+    @property
+    def datasheet(self):
+        if self.context.datasheet:
+            return self.context.datasheet
+        return self.parent.datasheet
+
+    @property
+    def related_items(self):
+        context = self.context
+        if hasattr(context, 'relatedItems'):
+            if context.relatedItems:
+                return context.relatedItems
+        parent = self.parent
+        if hasattr(parent, 'relatedItems'):
+            if parent.relatedItems:
+                return parent.relatedItems
+        return None
--- bda/plone/productshop/indexer.py
+++ bda/plone/productshop/indexer.py
@@ -1,1 +1,27 @@
-#
+from plone.indexer import indexer
+from .behaviors import (
+    IColorBehavior,
+    IWeightBehavior,
+    ISizeBehavior,
+    IDemandBehavior,
+)
+
+
+@indexer(IColorBehavior)
+def color_aspect(obj):
+    return obj.color
+
+
+@indexer(IWeightBehavior)
+def weight_aspect(obj):
+    return obj.weight
+
+
+@indexer(ISizeBehavior)
+def size_aspect(obj):
+    return obj.size
+
+
+@indexer(IDemandBehavior)
+def demand_aspect(obj):
+    return obj.demand
