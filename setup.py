1d15b8bb44b55686f1bd4689768aa6f1065bb6cd
benniboy <bs@kleinundpartner.at>
2014-12-18 11:07:47 +0100

    minor cleanup

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
-import os
-from setuptools import setup
 from setuptools import find_packages
-
+from setuptools import setup
+import os
 
 version = '0.9.dev0'
 shortdesc = "Product shop extension for bda.plone.shop"
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'CHANGES.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'LICENSE.rst')).read()
-
 
 setup(
     name='bda.plone.productshop',
@@ -26,7 +24,7 @@ setup(
     author_email='dev@bluedynamics.com',
     license='GNU General Public Licence',
     packages=find_packages('src'),
-    package_dir = {'': 'src'},
+    package_dir={'': 'src'},
     namespace_packages=['bda', 'bda.plone'],
     include_package_data=True,
     zip_safe=False,
@@ -49,4 +47,4 @@ setup(
     [z3c.autoinclude.plugin]
     target = plone
     """,
-    )
+)
